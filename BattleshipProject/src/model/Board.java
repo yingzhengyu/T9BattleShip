fc837aa84cc0fd59dd0841c5cadaaecc87522a07
Justin Barragan <justinbarragan1@gmail.com>
2021-04-25 18:16:56 -0700

    added input logic for board

--- BattleshipProject/src/model/Board.java
+++ BattleshipProject/src/model/Board.java
@@ -4,11 +4,19 @@ public class Board {
 	
 	int[][] grid = new int[8][8];
 	
-	public void updateGrid(char x, int y) {
+	public static void updateGrid(char x, int y) {
+		// converting char ascii into integer 1-8
+		int charAscii = x;
+		charAscii -= 96;
+		System.out.println(charAscii);
 		
 	}
 
 	public int[][] getGrid() {
 		return this.grid;
+	}
+	
+	public static void main (String[] args) {
+		updateGrid('a', 1);
 	}
 }
