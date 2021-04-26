c1e788f7181044e1ded46c1ff04db4bb7afcad12
Ebadullah-Wardak <72373857+Ebadullah-Wardak@users.noreply.github.com>
2021-04-25 18:26:28 -0700

    Add files via upload

---
+++ BattleshipProject/src/model/Boat.java
@@ -0,0 +1,13 @@
+
+public class Boat {
+	private int coordinate;
+	private boolean boatDamaged;
+	private int[] boatCoordinate;
+	
+	public Boat(int[] coordinate) {
+		
+	}
+	public void setDamagedBoat(Boat b) {}
+	public boolean boatAlive() {return true;}
+
+}
