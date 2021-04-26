5e5d861c00fb528c1238f619662dae8788060d55
Kevin Chang <kchang821126@gmail.com>
2021-04-25 19:22:37 -0700

    update view

--- BattleshipProject/src/view/View.java
+++ BattleshipProject/src/view/View.java
@@ -1,5 +1,23 @@
 package view;
-
+ 
+import model.Board;
 public class View {
+	
+	int[][] grid = new int[8][8];
+	
+	public static void printGrid(int[][] grid) {
+		
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				System.out.print(grid[i][j] + " ");
+			}
+		}
+		System.out.println("");
+	}
+	
+	
+	public static void main (String[] args) {
+		printGrid(new int[8][8]);
+	}
 
 }
