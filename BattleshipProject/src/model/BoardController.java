27ec3b2c6e381c7f47641fcd7ee4881e2c041da5
Justin Barragan <justinbarragan1@gmail.com>
2021-04-25 21:40:46 -0700

    added logic to boardcontroller

--- BattleshipProject/src/model/BoardController.java
+++ BattleshipProject/src/model/BoardController.java
@@ -1,5 +1,27 @@
 package model;
 
+import view.View;
+
 public class BoardController {
+	
+	private Board model;
+	private View view;
+	
+	public BoardController(Board model, View view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void action(char x, int y) {
+		model.updateGrid(x, y);
+	}
+	
+	public int[][] getGrid() {
+		return model.getGrid();
+	}
+	
+	public void updateView() {
+		view.printGrid(model.getGrid());
+	}
 
 }
