03e7a4b01119b184a55df183b1ac6139b87b00c2
Ebadullah-Wardak <72373857+Ebadullah-Wardak@users.noreply.github.com>
2021-04-25 18:05:29 -0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,34 @@
+
+import model.Tile;
+public class Player {
+	private String PlayerName;
+	private Tile Health;
+	
+	private Tile attackTile(int x, int y) {
+		return new Tile();
+	}
+	
+//	private int DisplayHealth(Boat b) {
+//		
+//	}
+	
+	public String getPlayerName() {
+		return this.PlayerName;
+	}
+	
+	public void setPlayerName(String n) {
+		this.PlayerName = n;
+	}
+	
+//	private Tile placeBoat(Boat b) {
+//		
+//	}
+	
+//	private Tile setTile(int x,int y) {
+//		
+//		return new Tile();
+//	}
+	
+	
+
+}
