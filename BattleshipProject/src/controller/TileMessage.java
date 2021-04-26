2d8ea58fc16d9aa4f4bd93e799ece16c82bdb7a0
Ebadullah-Wardak <72373857+Ebadullah-Wardak@users.noreply.github.com>
2021-04-25 18:47:50 -0700

    Add files via upload

---
+++ BattleshipProject/src/controller/TileMessage.java
@@ -0,0 +1,20 @@
+package model;
+
+public class TileMessage {
+	char x;
+	int y;
+	public TileMessage(char x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public char getXCoordinate() {
+		return this.x;
+	}
+	
+	public int getYCoordinate() {
+		return this.y;
+	}
+
+	
+}
