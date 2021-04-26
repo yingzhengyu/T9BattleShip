4648ad8553a5239276704814787f7a66d4780d32
Justin Barragan <justinbarragan1@gmail.com>
2021-04-25 18:07:33 -0700

    changes to board + view

--- BattleshipProject/src/model/Board.java
+++ BattleshipProject/src/model/Board.java
@@ -3,5 +3,12 @@ package model;
 public class Board {
 	
 	int[][] grid = new int[8][8];
+	
+	public void updateGrid(char x, int y) {
+		
+	}
 
+	public int[][] getGrid() {
+		return this.grid;
+	}
 }
