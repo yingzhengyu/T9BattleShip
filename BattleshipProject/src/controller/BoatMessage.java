e17f388cf2edcac23d86740dbb9dcc656e5e0f90
Andrew Yu <yingzheng.yu@sjsu.edu>
2021-04-25 21:50:49 -0700

    BoatMessage modified

--- BattleshipProject/src/controller/BoatMessage.java
+++ BattleshipProject/src/controller/BoatMessage.java
@@ -1,5 +1,22 @@
 package controller;
 
 public class BoatMessage {
-
+	int[] fourTiltBoat = new int [3]; // 0,1,2,3 element so health is 4
+	int[] fiveTiltBoat = new int [4];
+	
+	
+	public int[] getFourTiltBoat() {
+		return fourTiltBoat;
+	}
+	public void setFourTiltBoat(int[] fourTiltBoat) {
+		this.fourTiltBoat = fourTiltBoat;
+	}
+	public int[] getFiveTiltBoat() {
+		return fiveTiltBoat;
+	}
+	public void setFiveTiltBoat(int[] fiveTiltBoat) {
+		this.fiveTiltBoat = fiveTiltBoat;
+	}
+	
+	
 }
