d7999c7e9d0ecc2a3471788ae80a044c98ee1ac6
Justin Barragan <justinbarragan1@gmail.com>
2021-04-25 22:05:08 -0700

    Added logic to demoboard to simulate attacks on the command line

--- BattleshipProject/src/model/DemoBoard.java
+++ BattleshipProject/src/model/DemoBoard.java
@@ -9,13 +9,25 @@ public class DemoBoard {
 		View gameView = new View();
 		BoardController controller = new BoardController(gameBoard, gameView);
 		
+		controller.getGrid()[1][4]=2;
+		controller.getGrid()[3][6]=2;
+		
 		System.out.println("Initial board:");
 		controller.updateView();
+		
 		
 		// Attacking a2:
 		controller.action('a',2);
 		// Attacking e5:
 		controller.action('e', 5);
+		
+		// Attacking b5:
+		controller.action('b', 5);
+		// Attacking d7:
+		controller.action('d', 7);
+		
+		// no additional changes are made when attacking the same tile multiple times: 
+		controller.action('d', 7);
 	}
 	
 
