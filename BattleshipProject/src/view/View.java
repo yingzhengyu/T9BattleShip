28c02352f1d2d07b3397820466564ce00cc2dd8e
Justin Barragan <justinbarragan1@gmail.com>
2021-05-09 16:28:52 -0700

    Created a second grid for player 2: buildGrid is now buildGrids (method in View)

--- BattleshipProject/src/view/View.java
+++ BattleshipProject/src/view/View.java
@@ -9,29 +9,45 @@ import model.Tile;
 public class View extends JFrame {
 	// Stores a grid of integers to represent the game board
 	Tile[][] grid = new Tile[8][8];
-	JPanel panel = new JPanel();
+	JPanel outerPanel = new JPanel(new GridLayout(1,2,60,5));
+	JPanel leftPanel = new JPanel();
+	JPanel rightPanel = new JPanel();
 	BlockingQueue<Message> queue;
 	
 	public View(BlockingQueue<Message> queue) {
 		this.queue = queue;
 		
-		buildGrid();
-	}
-	
-	private void buildGrid() {
-		this.setSize(1000, 1000);
+		this.setSize(1400, 1000);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.panel.setLayout(new GridLayout(8,8));
+		
+		buildGrids();
+		this.setVisible(true);
+	}
+	
+	private void buildGrids() {
+
+		this.leftPanel.setLayout(new GridLayout(8,8,5,5));
 		
 		for (int i = 0; i <= 7; i++) {
 			for (int j = 0; j <= 7; j++) {
 				grid[i][j] = new Tile();
-				panel.add(grid[i][j]);
+				leftPanel.add(grid[i][j]);
 			}
 		}
-		this.add(panel);
-		this.setVisible(true);
+		this.outerPanel.add(leftPanel);
+		
+		this.rightPanel.setLayout(new GridLayout(8,8,5,5));
+		
+		for (int i = 0; i <= 7; i++) {
+			for (int j = 0; j <= 7; j++) {
+				grid[i][j] = new Tile();
+				rightPanel.add(grid[i][j]);
+			}
+		}
+		this.outerPanel.add(rightPanel);
+		
+		this.add(this.outerPanel);
 	}
 
 }
