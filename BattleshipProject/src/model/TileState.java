4dd9bf93fd919867f251337bdf4d589db982375e
Ebadullah-Wardak <72373857+Ebadullah-Wardak@users.noreply.github.com>
2021-05-10 18:53:57 -0700

    Add files via upload
    added an enumerated types of ships

--- BattleshipProject/src/model/TileState.java
+++ BattleshipProject/src/model/TileState.java
@@ -1,10 +1,38 @@
 package model;
 
 public enum TileState {
-	HIDDEN,
-	EMPTY_NOT_HIT,
-	EMPTY_HIT,
-	OCCUPIED_NOT_HIT,
-	OCCUPIED_HIT,
-	DEFEATED
+	HIDDEN(-1),
+	EMPTY_NOT_HIT(0),
+	EMPTY_HIT(1),
+	OCCUPIED_NOT_HIT(2),
+	OCCUPIED_HIT(3),
+	DEFEATED(4);
+	
+	private int Action;
+	
+	private TileState(int action) {
+		this.Action = action;
+	}
+	public int getAction() {
+		return this.Action;
+	}
+	
+	public enum TypeofBoats{
+		Carrier(5),
+		Battleship(4),
+		Crusier(3),
+		Submarine(3),
+		Destroyer(2);
+		
+		
+		private int size;
+		
+		private TypeofBoats(int s){
+			this.size = s;
+		}
+		
+		public int getSize() {
+			return size;
+		}
+	}
 }
