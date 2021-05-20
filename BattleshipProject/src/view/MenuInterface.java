49829186ec863053326186727683376027c63eb7
Kevin Chang <kchang821126@gmail.com>
2021-05-19 19:31:56 -0700

    added back background and change setting to quit button

--- BattleshipProject/src/view/MenuInterface.java
+++ BattleshipProject/src/view/MenuInterface.java
@@ -3,6 +3,7 @@ package view;
 import javax.swing.*;
 
 import controller.Message;
+import controller.QuitMessage;
 import controller.StartMessage;
 
 import java.awt.*;
@@ -15,10 +16,10 @@ import java.util.concurrent.BlockingQueue;
 public class MenuInterface extends JFrame implements Window{
 
     JButton start = new JButton("Start");
-    JButton setting = new JButton("Setting");
+    JButton quit = new JButton("Quit");
     JPanel title = new JPanel();
-    //ImageIcon img = new ImageIcon(".\\Images\\Background.jpg");
-    //JLabel background = new JLabel("",img, JLabel.CENTER);
+    ImageIcon img = new ImageIcon(".\\Images\\Background.jpg");
+    JLabel background = new JLabel("",img, JLabel.CENTER);
     JLabel textfield = new JLabel();
     GridBagConstraints gbc = new GridBagConstraints();
 /**
@@ -34,8 +35,8 @@ public class MenuInterface extends JFrame implements Window{
         this.getContentPane().setBackground(Color.BLACK);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //this.add(background);
-        //background.setOpaque(true);
+        this.add(background);
+        background.setOpaque(true);
 
         textfield.setForeground(Color.WHITE);
         textfield.setFont(new Font("normalFont", Font.BOLD,75));
@@ -58,24 +59,31 @@ public class MenuInterface extends JFrame implements Window{
             }
         });
 
-        setting.setFont(new Font("normalFont", Font.PLAIN, 50));
-        setting.setBounds(0,0,200,100);
+        quit.setFont(new Font("normalFont", Font.PLAIN, 50));
+        quit.setBounds(0,0,200,100);
+        quit.addActionListener(e -> {
+            try {
+            	queue.put(new QuitMessage());
+            } catch (InterruptedException exception) {
+                // do nothing
+            }
+        });
         
         
-        this.setLayout(new GridBagLayout());
-        //background.setLayout(new GridBagLayout());
+        //this.setLayout(new GridBagLayout());
+        background.setLayout(new GridBagLayout());
         gbc.insets = new Insets(0,5,100,5);
         gbc.gridx = 0;
         gbc.gridy = 30;
         gbc.fill = GridBagConstraints.VERTICAL;
-        this.add(title, gbc);
-        //background.add(title,gbc);
+        //this.add(title, gbc);
+        background.add(title,gbc);
         gbc.gridy = 60;
-        this.add(start, gbc);
-        //background.add(start,gbc);
+        //this.add(start, gbc);
+        background.add(start,gbc);
         gbc.gridy = 120;
-        this.add(setting, gbc);
-        //background.add(setting,gbc);
+        //this.add(setting, gbc);
+        background.add(quit,gbc);
         
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
